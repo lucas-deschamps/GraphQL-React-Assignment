996e616de899075f163d8a7f0919bd2b668b777a
Lucas Deschamps <l-deschamps@outlook.com>
2021-09-09 23:49:17 -0300

    initial modelling of schema

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,41 @@
 import { ApolloServer, gql } from "apollo-server";
 
 const typeDefs = gql`
-  
+  scalar Date
+
+  enum InsuranceType {
+    Liability
+    Household
+    Health
+  }
+
+  enum PolicyStatus {
+    Active
+    Pending
+    Cancelled
+    Dropped out
+  }
+
+  type Customer {
+    firstName: String!
+    lastName: String!
+    dateOfBirth: Date!
+  }
+
+  type Policy {
+    customer: Customer!
+    provider: String!
+    insuranceType: InsuranceType!
+    status: PolicyStatus!
+    policyNumber: String!
+    startDate: Date!
+    endDate: Date!
+    createdAt: Date!
+  }
+
+  type Query {
+    customers: [Customer]
+  }
 `;
 
 const resolvers = {};
