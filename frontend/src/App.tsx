3c40a83feb67f64fd76a0c3917661b2a40542e3d
Lucas Deschamps <l-deschamps@outlook.com>
2021-09-12 18:02:53 -0300

    adding and configuring apollo client

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,20 @@
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+import { GRAPHQL_API_URL } from './config';
+
 import "./index.css";
 
 function App() {
-  return <h1 className="text-3xl">Fullstack code challenge</h1>;
+  const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    uri: GRAPHQL_API_URL,
+  });
+
+  return (
+    <ApolloProvider client={client}>
+      <div>WIP</div>
+    </ApolloProvider>
+  );
 }
 
 export default App;
---
+++ frontend/src/config.ts
@@ -0,0 +1,1 @@
+export const GRAPHQL_API_URL: string = 'http://localhost:4000/graphql';
