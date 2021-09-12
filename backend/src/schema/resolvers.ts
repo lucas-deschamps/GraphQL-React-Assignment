2a9baab283be34a89cb085ae43efe940a0be63ce
Lucas Deschamps <l-deschamps@outlook.com>
2021-09-12 18:39:09 -0300

    adding the 'customers' resolver to back end

--- backend/src/schema/resolvers.ts
+++ backend/src/schema/resolvers.ts
@@ -1,10 +1,13 @@
 import { GraphQLDateTime, GraphQLDate } from 'graphql-iso-date';
 
+import { customers } from '../db/customers';
+import { PolicyStatus, InsuranceType, policies } from '../db/policies';
+
 const resolvers = {
   Date: GraphQLDate,
   DateTime: GraphQLDateTime,
   Query: {
-    //
+    customers: () => customers,
   },
 };
 
